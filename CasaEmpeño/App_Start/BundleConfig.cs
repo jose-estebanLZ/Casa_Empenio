64f2aea1c02fc5f9bc8c2daa9dc6291756d81bf3
José Esteban <jose@datarial.com>
2024-03-07 17:53:24 -0600

    Se incluyó el tema HOMER y se creo la arquitectura general del proyecto

--- CasaEmpeño/App_Start/BundleConfig.cs
+++ CasaEmpeño/App_Start/BundleConfig.cs
@@ -8,23 +8,46 @@ namespace CasaEmpeño
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // Homer style
+            bundles.Add(new StyleBundle("~/bundles/homer/css").Include(
+                      "~/Content/style.css", new CssRewriteUrlTransform()));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            // Homer script
+            bundles.Add(new ScriptBundle("~/bundles/homer/js").Include(
+                      "~/Vendor/metisMenu/dist/metisMenu.min.js",
+                      "~/Scripts/homer.js"));
 
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            // Animate.css
+            bundles.Add(new StyleBundle("~/bundles/animate/css").Include(
+                      "~/Vendor/animate.css/animate.min.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            // Pe-icon-7-stroke
+            bundles.Add(new StyleBundle("~/bundles/peicon7stroke/css").Include(
+                      "~/Icons/pe-icon-7-stroke/css/pe-icon-7-stroke.css", new CssRewriteUrlTransform()));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            // Font Awesome icons style
+            bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include(
+                      "~/Vendor/fontawesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
+
+            // Bootstrap style
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+                      "~/Vendor/bootstrap/dist/css/bootstrap.min.css", new CssRewriteUrlTransform()));
+
+            // Bootstrap
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include(
+                      "~/Vendor/bootstrap/dist/js/bootstrap.min.js"));
+
+            // jQuery
+            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include(
+                      "~/Vendor/jquery/dist/jquery.min.js"));
+
+            // jQuery UI
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui/js").Include(
+                      "~/Vendor/jquery-ui/jquery-ui.min.js"));
+
+            // jQuery Validation
+            bundles.Add(new ScriptBundle("~/bundles/validation/js").Include(
+                      "~/Vendor/jquery-validation/jquery.validate.min.js"));
         }
     }
 }
---
+++ CasaEmpeñoModel/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasaEmpeñoModel.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
