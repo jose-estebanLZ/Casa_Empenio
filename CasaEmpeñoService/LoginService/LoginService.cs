bbbab3df1301f969930583ff9c190f7154d43060
José Esteban <jose@datarial.com>
2024-03-10 09:14:12 -0600

    Encriptación MD5 para el login

--- CasaEmpeñoService/LoginService/LoginService.cs
+++ CasaEmpeñoService/LoginService/LoginService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +16,24 @@ namespace CasaEmpeñoService.LoginService
             _userRepository = new UserRepository();
         }
 
-        public bool IsValidUser(string userName, string password) =>
-            _userRepository.Get(userName, password) != null;
+        public bool IsValidUser(string userName, string password)
+        {
+            var encryptedPassword = Encrypt.GetMD5(password);
+            return _userRepository.Get(userName, encryptedPassword) != null;
+        }
+           
+    }
+}
+
+public class Encrypt
+{
+    public static string GetMD5(string str)
+    {
+        MD5 md5 = MD5CryptoServiceProvider.Create();
+        ASCIIEncoding encoding = new ASCIIEncoding();
+        StringBuilder sb = new StringBuilder();
+        byte[] stream = md5.ComputeHash(encoding.GetBytes(str));
+        for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+        return sb.ToString();
     }
 }
